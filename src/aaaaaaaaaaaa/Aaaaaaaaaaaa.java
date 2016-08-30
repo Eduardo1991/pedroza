2d935822ad52862d60b047cc37e0c036e267b17d
Eduardo1991 <elvergalarga19910000@gmail.com>
2016-08-30 17:13:49 -0500

    segunda modificasion

--- src/aaaaaaaaaaaa/Aaaaaaaaaaaa.java
+++ src/aaaaaaaaaaaa/Aaaaaaaaaaaa.java
@@ -7,11 +7,13 @@ public class Aaaaaaaaaaaa {
     public static void main(String[] args) {
      ///esto es una prueba
         String eduardo;
-        
+        int numero;
+        //esto es la segunda modificasion
     }
 
     public Aaaaaaaaaaaa() {
-  
     }
+
+    
     
 }
